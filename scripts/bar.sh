95a1bc27df0f3c73541ebf8faa66db29e23b0cc5
tatosaurus <tato@uribe.com.mx>
2014-11-05 18:17:42 -0600

    added my .Xdefaults

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -1,2 +1,2 @@
 ## Make changes to the size of the bar depending on the resolution of your display
-./bar_mk.sh | bar -p -g 1348x26+10+10 -f -*-gohufont-medium-*-*-14-*-*-*-*-iso10646-* -B#FF151515
+./dotfiles/scripts/bar_mk.sh | bar -p -g 1348x26+10+10 -f -*-gohufont-medium-*-*-14-*-*-*-*-iso10646-* -B#FF151515 &
